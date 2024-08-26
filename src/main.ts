4ebdaf699154480a617d099c5eede1a20e44c9d0
Vivek Jain <vivek.jain@intelliswift.com>
2024-08-26 23:42:11 +0530

    AG Grid

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import 'ag-grid-community/styles/ag-grid.css'; /* Core Data Grid CSS */
+import 'ag-grid-community/styles/ag-theme-quartz.css'; /* Quartz Theme Specific CSS */
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
